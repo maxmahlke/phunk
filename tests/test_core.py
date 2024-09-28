4feb9410ebe058b2726fd6d4fe808dc0236c4fe1
Max Mahlke <max.mahlke@rwth-aachen.de>
2024-09-28 17:04:11 +0200

    Test cases for (multi-band) model fitting

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,7 +20,7 @@ OBS_8988 = pd.read_csv(Path(__file__).parent / "data/Hansenkoharcheck.csv")
         ("HG1G2", 6.435448791494396),
         ("HG12", 6.47026229130713),
         ("HG12S", 6.4186901600356965),
-        # ("sHG1G2", 6.430901601343539),
+        ("sHG1G2", 6.435448793849064),
         ("LinExp", -3730.422348715857),
     ],
 )
@@ -30,7 +30,11 @@ def test_fitting_models(model, hmag_expected):
     phase = PHASE
     mag = MAG
 
-    pc = phunk.PhaseCurve(phase=phase, mag=mag)
+    # For sHG1G2
+    ra = [184.568391, 184.338311, 183.433022, 180.361071, 179.244605, 178.693809]
+    dec = [-2.467547, -2.361879, -1.944443, -0.500051, 0.048319, 0.410682]
+
+    pc = phunk.PhaseCurve(phase=phase, mag=mag, ra=ra, dec=dec)
     pc.fit(models=[model])
 
     np.testing.assert_almost_equal(
@@ -40,9 +44,42 @@ def test_fitting_models(model, hmag_expected):
 
 def test_multi_band():
     """Test instantiation and fitting of multi-band phase curves."""
-    pc = phunk.PhaseCurve(phase=OBS_8988.phase, mag=OBS_8988.mred, band=OBS_8988.band)
+
+    # Using local cache of ATLAS phase curve of (8988) 1979 MA4
+    pc = phunk.PhaseCurve(
+        phase=OBS_8988.phase,
+        mag=OBS_8988.mred,
+        band=OBS_8988.band,
+        ra=np.degrees(OBS_8988.ra),
+        dec=np.degrees(OBS_8988.dec),
+    )
 
     assert pc.bands == ["c", "o"]
 
+    pc.fit(models=["HG1G2", "sHG1G2"])
 
-# different bands -> when fitting any model, parameters get band suffix
+    # ------
+    # HG1G2
+
+    # Assert that we have band specific parameters
+    for param in ["Hc", "Ho", "G1c", "G1o", "G2c", "G2c"]:
+        assert hasattr(pc.HG1G2, param)
+
+    # Assert that we do not have general parameters
+    for param in ["H", "G1", "G2"]:
+        assert not hasattr(pc.HG1G2, param)
+
+    # ------
+    # sHG1G2
+
+    # Assert that we have band specific parameters
+    for param in ["Hc", "Ho", "G1c", "G1o", "G2c", "G2c"]:
+        assert hasattr(pc.sHG1G2, param)
+
+    # Assert that we do not have general parameters
+    for param in ["H", "G1", "G2"]:
+        assert not hasattr(pc.sHG1G2, param)
+
+    # Except for the shape
+    for param in ["R", "alpha", "delta"]:
+        assert hasattr(pc.sHG1G2, param)
